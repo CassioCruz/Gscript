e6a07092337dedd1f0e28b1c57ba9e1deb21f42f
CassioCruz <cassiosousa1997@gmail.com>
2021-12-28 14:51:51 +0100

    beta

--- install.sh
+++ install.sh
@@ -3,42 +3,40 @@ clear
 printf '\033]2;INSTALLER\a'
 echo -e "Press \e[1;33many key\e[0m to install the script..."
 read -n 1
-clear
-apt-get -y install gnome-terminal
-clear
+apt-get -y install gnome-terminal || clear
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "$DIR" != "/root/gscript" ]]
-then
-	sleep 2
-	if [[ -d /root/gscript ]]
 	then
-		rm -r /root/gscript
+		#gnome-terminal -- /root/gscript/gscript.sh
+		sleep 2
+	if [[ -d /root/gscript ]]
+		then
+			rm -r /root/gscript
 	fi
-	mkdir /root/gscript
-	cp -r "$DIR"/* /root/gscript
-	chmod +x /root/gscript/install.sh
-	#gnome-terminal -- /root/gscript/install.sh
+		mkdir /root/gscript
+		cp -r "$DIR"/* /root/gscript
+		chmod +x /root/gscript/install.sh
 fi
-echo -e "Installing gscript..."
+echo -e "    Installing gscript..."
 sleep 1
-#echo -e "Fixing permissions"
+echo -e "\n\n Fixing permissions"
 sleep 2
-clear
+chmod +x /root/gscript/gscript.sh
+chmod +x /root/gscript/install.sh
+chmod +x /root/gscript/LICENSE.md
+#clear
 echo -e "Copying script to /bin/gscript"
 sleep 1
 mkdir /bin/gscript
 cd /root/gscript
 cp /root/gscript/gscript.sh /bin/gscript
-clear
-#required for gui
-apt-get -y install ncurses-dev
-clear
+cp /root/gscript/install.sh /bin/gscriptn
 if [[ ! -d /root/handshakes ]]
 then
 	mkdir /root/handshakes
-	echo -e "Made /root/handshake directory"
+	echo -e "Creating /root/handshake directory"
 else
-	echo -e "/root/handshakes directory detected.Good."
+	echo -e "/root/handshakes directory available"
 fi
 if [[ ! -d /root/wordlists ]]
 then
@@ -50,15 +48,10 @@ else
 	sleep 1
 fi
 	echo -e "\n\nAdding gscript to PATH so you can access it from anywhere"
-	sleep 1
-	sleep 1
-	clear
-	break
-clear
+	sleep 2
 echo -e "\nAlmost...\nDONE."
 sleep 1
 echo -e "\n\nOpen a NEW terminal and type 'g' to launch the script"
 echo "alias g="/bin/gscript/gscript.sh"" >> $HOME/.bashrc
 sleep  2
-gnome-terminal -- /root/The_Lola_Script/Gscript/gscript.sh
 exit
