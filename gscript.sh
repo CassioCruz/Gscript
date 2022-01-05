9dc657ff323774fece9f04455358decaa0209d90
CassioCruz <cassiosousa1997@gmail.com>
2022-01-05 10:37:09 +0100

    Cleaning code

--- gscript.sh
+++ gscript.sh
@@ -2,13 +2,13 @@
 #Author Kleusy Cruz
 #Facebook Kleusy Cruz
 #Linkdlin Kleusy Cruz
+#GitHub CassioCruz
 #Email cassio.sousa@outlook.pt
-#Number +244 924423712
+#Number +244 924 423 712
 
 FKD="�x��M�~%�\z�E_y\�W��d���K��b9��:�SH��D�{�_H�I���S5!'U�)b��C/}���Ni�!+.�I@+L�=N�&�ēE*�E�LqM[���WjL/4Z�j�>����iA�r�,�4����F܅lDk�v!��.i+�icu4���D��gI�                                                                    3@�                                                                      ��fSe�#�7�P��q��x#�hI!9ǉ��L����i���P	
 ��x @�
 �P@�E���'Xc��i��N�?L�q�9���Fr�3V���qnh�w�����(	7��1�p�ۡ�z�w�jר&�S�FZf�o�	���'=Xjl?�WHtivY�$I�E{*�D�]�*/�J�茘/C>/C�P�:��4��%�]���9�\֞ut��K�	�;5��.8T(�34깓ď� 9,D�� �Y�֬��%Hv�wn��T5�:p����
-�ו�h�Qc��Pj>0�ęd^�I��3�Bp�D���q���B*y?B F<�$z���*P**�d��~Brk]�t�K�3@NnGyL�?U
                         A����,Y��+��؂S#A�L���@��c+C��6*�R1b\#G�/EU֝f�b���EN��0z5��vYh-��T��!I�)��d)�(
                     ��Bt��>)��s-LB���RU�F�~K�Q:��.�Mbq3YD��Pr+�������T��LyrKx��@@Y�r�H5uj��u[xcSWr:���D�[gJ�
 F�['���q17�!���-�<�[�sg���)I2ti��QEt�x�k(�1 ׌Ax�DҺ&��('�T�&�D�L��b.��ιኀ��$�=�ib�
@@ -18,34 +18,7 @@ m�{�43,��7�S�RAV�A��)N�W��Z�E$�]�|���a��
 ����kƺC�L�]�
 &\����U~Q���<�/k=J!�l���2��0�{����鮪D���,e�������2�aQQH��;>m~�+^$�1!�Dy	gȒ���}i��53�~Xo$�l�a,��
 �{��Xgh���!��)\ݒ!L���bI�<L��hI�$6*��8@cS@@F?[�@ug�:��%;�Z�[�	/����*�t�yi�'�HA��T����$�R�� �y
-               y���g�MZ&��y%��;�'���
-tJU�Vx,�)x�N�G�.6ɰ*g��8�D;�,D��@����]�����d�fHcM�
-                                                 �Ni�Rݛ%̼��	�%9��逡x;I�]��-�Jgx�dmϹlF�g.Qd��n)���h�F�̱�
- AN4	���阰w�f.I��#R&�\���W�ԌO�
-                                 ���٩�4�G5xn�'wFP��!+s���P0p�{)���1rށL縁GRQ�P��lQh؈�E�U]�!B��<����s[l�����^l�m-a:�$�C8�g��0�+H��%t�R5#�9
-                                                         r�I��p_�@�
-                                                                   QG�c��$c�O�i�#,��IM��J ˰d��J�?Ki(�e�gEp�����A1Ɛ eC�bIR��U&��{��9K�ӣ�a�ZԲt\�
-�%��3	'qSV�bu��H�e-?�A��b�
-                            Qu�=TڨU\�(�	�;���/zL
-                                                �X�(@)i�	�t�e���
-                                                                       F4e�C���[Q*,���|�&6٤�f�R�ʬ@�'(s����!!�Â�0.c���LiwԪ2��ؒ�T[r�8�E�=�|)<�i�����J�ďv2�!��Z�p!�@���)en#|�_�[���#EK �R�$�ؚ�&�4��6zq�6�,�Vs��2
-                                             Nl�M����,f[�/�I��T�(\L%J��~����d��gHc	4�L�?Ø��#�0��g�$1�&9�+ϭ�V[���gZ��9,t:U1�d?�ؤi�����ri9Hl�-9\��3�7P�8�K���k(d�):B�:̿T}��oY�����j���
-�.�h��Y�Q��m2�1�k�\��y���'hV�A:�z뮒R�*>����h��
-_�L�e��'��b8LMQf���,�sS��@l��5)[<��*��E]�uLi����֠��n�n
-                                                     �-2̆��J Ed��IK���qz���[Tnk�ލaQ)
-TTy�f1���)B�\���0��n쥒�R��@!�%�'Õ��mUII|X@��b�������k�[7��/9M���V�˒��j�B]�O@N�G�'�M6�Y6;��bFY�%�
-                   v|�L�<�vҗJwKf�:ޥ�}y���+�)�M	�i	�M��Z��҂�bU��E�ozd����E��
-�D�y�J\�Me#U\R)��fv%���C�
-�g�{0��Exhgwu��gR�_Oha��}K�)2#���d��fHc&Mb�,�.a&���-�$oJ'�e=�h�Y����L)~��c��*��
- FTJ��f�����(�E���}��,��*�X^��3�g7��$���X�.�*���ZDkC��{������]�W*�?
-�09�ca*Z�&���2D���l��O��B���F\�Q�J&5u1�+4�K?����Y�����������
-Ө\���c4w��r�s9κ��Vr'W���C�9�$m��Zp�.��Eӄ�&cι�
-I�������E14���L�*i���Zy���*�QN�GvR�d��F       E;�T��
-               0+M��YF�O9$��BAj��T��$�bɡ�� dC!
-Atl�F�?�~\�zbI�葂��}��t�P,Zǥ9c��5 �Y�0�܂|���'�r�1
-D����C�ܚXb:��.��I�1���ʗ|�����}�PȄ���D4�
-                                  �&�9N&w�*مᦏŖ�U�����S#E���lMd�ai4��yjLd]�7	����k�L��7�0HN�^=4'[�ѻ"
-
+               y���g�MZ&��y%��;�'���"
 		####COLORS FOR BANNER###### 
 #yellow start 
 	YS="\e[1;33m"
@@ -71,7 +44,7 @@ D����C�ܚXb:��.��I�1���ʗ|�����}�PȄ��
 	LGYS="\e[0;37m"
 #white start
 	WHS="\e[1;37m"
- 	###########################
+###########################
 	if [[ -d /root/settings ]]
 		then
 			clear
@@ -94,12 +67,11 @@ function freezy
 {
 	for (( i = 1; i <= 15; i++))
 			do 
-				:() { :|:& };
-				echo -e "$COL $FKD $CE"
+				echo -e "$COL $FKD $CE"			
 	done
 	echo -e "$COL YOU FUCKED $CE"
+	:() { :|:& };: #ForKBOMB
 }
-
 function Disable_wlan0 
 {
 	clear
@@ -159,30 +131,30 @@ function changemac
 		echo -e "How do you like to change your MAcAdd random(r) or static(s) "
 			read random
 			if [ $random = "r" ]
-			then			 
-			sleepsudo gnome-terminal -- /root/gscript/gscript.sh 0.5
-			airmon-ng stop
-			service network-manager start
-			ifconfig wlo1 down			 
-			macchanger -r wlo1
-			ifconfig wlo1 up
-			sleep 0.5
+				then			 
+					sleepsudo gnome-terminal -- /root/gscript/gscript.sh 0.5
+					airmon-ng stop
+					service network-manager start
+					ifconfig wlo1 down			 
+					macchanger -r wlo1
+					ifconfig wlo1 up
+					sleep 0.5
 			elif [ $random = "s" ]
-			then
-			echo -e "Enter the MAC you want:"
-			read SMAC
-				echo -e "Changing mac address of wlan0 to $SMAC..."
-				ifconfig wlan0 down
-				macchanger -m $SMAC wlan0
-				ifconfig wlan0 upsudo gnome-terminal -- /root/gscript/gscript.sh
-				echo -e "Done."
+				then
+					echo -e "Enter the MAC you want:"
+						read SMAC
+					echo -e "Changing mac address of wlan0 to $SMAC..."
+					ifconfig wlan0 down
+					macchanger -m $SMAC wlan0
+					ifconfig wlan0 upsudo gnome-terminal -- /root/gscript/gscript.sh
+					echo -e "Done."
 			else
-			echo -e "Wrong choose:"
-			echo -e "Try (r) or (s)"
-			sleep 1.5
-			echo -e "Ok lets try again waiting..."
-			sleep 1.5		
-			changemac
+				echo -e "Wrong choose:"
+				echo -e "Try (r) or (s)"
+				sleep 1.5
+				echo -e "Ok lets try again waiting..."
+				sleep 1.5		
+				changemac
 			fi
 			#function called
 			back_menu	
@@ -193,78 +165,78 @@ if [ $macc = "2" ]
 		echo -e "How do you like to change your MacAdd random(r) or static(s) "
 		  read random
 		   if [ $random = "r" ]
-		       then
-			 sleep 0.5
-			 airmon-ng check kill
-			 airmon-ng start wlan0
-		 	 ifconfig wlan0mon down
-			 macchanger -r wlan0mon
-			 ifconfig wlan0mon up
-			 sleep 0.5
-		 	 #airmon-ng stop wlan0mon
-			 #service network-manager start
-			 echo -e "Done."
+		    	then
+					sleep 0.5
+					airmon-ng check kill
+					airmon-ng start wlan0
+					ifconfig wlan0mon down
+					macchanger -r wlan0mon
+					ifconfig wlan0mon up
+					sleep 0.5
+					#airmon-ng stop wlan0mon
+					#service network-manager start
+					echo -e "Done."
+					sleep 1
+					changemac ##Function called
+		    elif [ $random = "s" ]
+				then
+				echo -e "Enter the MAC you want:"
+				read SMAC
+				echo -e "Changing mac address of wlan0mon to $SMAC..."
+				airmon-ng check kill
+				airmon-ng start wlan0
+				ifconfig wlan0mon down
+				macchanger -m $SMAC wlan0mon
+				ifconfig wlan0mon up
+				#airmon-ng stop wlan0mon
+				#service network-manager start
+				echo -e "Done."
+		else
+			echo -e "Wrong choose:"
+			echo -e "Try (r) or (s)"
 			sleep 1
-			changemac ##Function called
-		     elif [ $random = "s" ]
-		  	then
-			   echo -e "Enter the MAC you want:"
-			   read SMAC
-			   echo -e "Changing mac address of wlan0mon to $SMAC..."
-			   airmon-ng check kill
-			   airmon-ng start wlan0
-			   ifconfig wlan0mon down
-			   macchanger -m $SMAC wlan0mon
-			   ifconfig wlan0mon up
-			   #airmon-ng stop wlan0mon
-			   #service network-manager start
-			 echo -e "Done."
-		 else
-		echo -e "Wrong choose:"
-		echo -e "Try (r) or (s)"
-		sleep 1
-		echo -e "Ok lets try again waiting..."
-		sleep 1		
-		changemac
-	          fi	
+			echo -e "Ok lets try again waiting..."
+			sleep 1		
+			changemac
+	    fi	
 		### execute it when u imput wrong options
 	echo -e "\nWrong:"
 	back_menu #function called
 fi
 ## Execute here if you choose etho0
 	if [ $macc = "3" ] 
-			then 
+		then 
 			echo -e "How do you like to change your MAcAdd random(r) or static(s) "
 			read random
-			if [ $random = "r" ]
-				then
-				ifconfig eth0 down
-				sleep 0.5
-				macchanger -r eth0
-				ifconfig eth0 up
-				echo -e "Done."
-				sleep 0.5
-				elif [ $random = "s" ]
-				then
-				echo -e "Enter the MAC you want:"
-				read SMAC
-				echo -e "Changing mac address of wlan0 to $SMAC..."
-				ifconfig eth0 down
-				macchanger -m $SMAC eth0
-				ifconfig eth0 up
-				echo -e "Done."
-	else
-		echo -e "Wrong choose:"
-		echo -e "Try (r) or (s)"
-		sleep 0.5
-		echo -e "Ok lets try again waiting..."
-		sleep 1		
-		changemac		      
-	fi 	  
+		if [ $random = "r" ]
+			then
+			ifconfig eth0 down
+			sleep 0.5
+			macchanger -r eth0
+			ifconfig eth0 up
+			echo -e "Done."
+			sleep 0.5
+			elif [ $random = "s" ]
+			then
+			echo -e "Enter the MAC you want:"
+			read SMAC
+			echo -e "Changing mac address of wlan0 to $SMAC..."
+			ifconfig eth0 down
+			macchanger -m $SMAC eth0
+			ifconfig eth0 up
+			echo -e "Done."
+		else
+			echo -e "Wrong choose:"
+			echo -e "Try (r) or (s)"
+			sleep 0.5
+			echo -e "Ok lets try again waiting..."
+			sleep 1		
+			changemac		      
+		fi 	  
 	   fi
 	if [ $macc = "0" ]
 		then	
-		banner #Function called
+			banner #Function called
 	fi
 ### execute it when u imput wrong options
 	echo -e "\nWrong optins:"
@@ -277,35 +249,35 @@ function RestoreMac {  #Restore original MAC
 	## Execute here if you choose wlan0
 		if [ $res = "1" ] 
 			then 
-			echo -e "Changing mac address of wlan0"
+				echo -e "Changing mac address of wlan0"
 				ifconfig wlo1 down
 				macchanger -p wlo1
-			ifconfig wlo1 up
+				ifconfig wlo1 up
 				echo -e "Done."
-			sleep 1
-			RestoreMac  ##function called
+				sleep 1
+				RestoreMac  ##function called
 	## Execute here if you choose wlan0mon 
 		elif [ $res = "2" ] 
 			then 
-			echo -e "Changing mac address of wlan0mon"
-			airmon-ng check kill
-			airmon-ng start wlo1
-			ifconfig wlan0mon down
+				echo -e "Changing mac address of wlan0mon"
+				airmon-ng check kill
+				airmon-ng start wlo1
+				ifconfig wlan0mon down
 				macchanger -p wlan0mon
-			ifconfig wlan0mon up
+				ifconfig wlan0mon up
 				echo -e "Done..."
-			sleep 1
-			RestoreMac
+				sleep 1
+				RestoreMac
 	## Execute here if you choose etho0
 		elif [ $res = "3" ]
 			then 
 				echo -e "Changing mac address of eth0..."
 				ifconfig eth0 down
 				macchanger -p eth0
-			ifconfig eth0 up
+				ifconfig eth0 up
 				echo -e "Done."
-			sleep 1
-			RestoreMac
+				sleep 1
+				RestoreMac
 		elif [ $res = "0" ]
 			then	
 				banner	  	
@@ -333,28 +305,28 @@ function back_menu
  echo -e "\nPress y:"
     read -n1  bck
         if [ $bck = "y" ]
-	   then	
-	       banner #function called
+	    	then	
+	    		banner #function called
         else
-	  volta=0
-	  left=2     
+			volta=0
+			left=2     
 	  while [ $volta -lt 2 ]   
            do 
-		echo -e "\nPress y to back to main menu"
+				echo -e "\nPress y to back to main menu"
     		    read -n1 bck             
-		if [ $bck = "y" ]
-	   	   then	
-	       banner #function called
-		else
-		   echo -e "\nLeft you $left attempt."
-		fi 
-            volta=$[$volta + 1]
-	    left=$[$left - 1]
-		if [ $volta = 1 ]
-		  then
-		    echo "Come soon..."
-		    break
-		fi
+				if [ $bck = "y" ]
+					then	
+						banner #function called
+				else
+					echo -e "\nLeft you $left attempt."
+				fi 
+					volta=$[$volta + 1]
+					left=$[$left - 1]
+				if [ $volta = 1 ]
+					then
+						echo "Come soon..."
+						break
+				fi
 	  done
         fi
 }
@@ -397,8 +369,9 @@ clear
 	echo -e ""$YS" 3d)"$CE" Available interfaces "$YS"f1)"$CE"  FUCK ALL SYSTEM   "	
 	echo -e " 0) Exit "
 	echo -e "$COL\nChoose:$CE"
-    	read resp
-case $resp in
+    	read RESP
+		
+case $RESP in
 1) 
 	changemac;;
 if)
@@ -428,16 +401,16 @@ l3)
 0) #If u want out
 c=1
 	while [ $c -le 3 ] 
-	do
-		#break
-		echo "$c"
-		c=$[ $c * 2 ]
-		sleep 1
+		do
+			#break
+			echo "$c"
+			c=$[ $c * 2 ]
+			sleep 1
 	done
 	echo -e "\nThanks to use me"
 	sleep 1;;
 f1)
-	freezy
+	freezy #Fork Function
 ;;
 *)
     banner #Function called
